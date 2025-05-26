1ee5ab19da8b2a6402b131d1f6233e0abab12261
RetroSuccess <165993473+RetroSuccess@users.noreply.github.com>
2025-05-26 15:13:52 +0200

    Update Question3.py

--- Assignment question/Question3.py
+++ Assignment question/Question3.py
@@ -1,19 +1,15 @@
-def draw_house():
-    # Draw the roof (width must match base = 9 characters)
-    for i in range(5):
-        stars = 2 * i + 1
-        spaces = 4 - i  # 4 spaces for the top line, decreasing
-        print(' ' * spaces + '*' * stars + ' ' * spaces)
-
-    # Draw the walls (height = 3, width = 9)
-    for i in range(3):
-        print('*' + ' ' * 7 + '*')
-
-    # Draw the base
-    print('*' * 9)
-
-# Call the function to draw the house
-draw_house()
-
+# The triangle 
+for row in range(5):
+    num_stars = 2 * row + 1
+    num_spaces = (9 - num_stars) // 2
+    print(' ' * num_spaces + '* ' * num_stars)
+    
+# square part
+for square_row in range(5):
+    if square_row == 4:
+        print('  ' + '* ' * 7)
+    else:
+        # Start with 2 spaces, then star, then 5 empty spaces, then star
+        print('  ' + '* ' + '  ' * 5 + '*')
 
 
